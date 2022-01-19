832b30ebdaeff91d2446a89130b71e5f9c709a7a
Minji Cho <mturtle051@gmail.com>
2022-01-18 16:09:27 -0800

    Completed prep-javascript-objects.

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -1,13 +1,15 @@
 var person = {
   firstName: "James",
   lastName: "Parker",
-  hobby: "Coding",
-  job: "Web Developer",
-  previousJob: "Student"
+  hobby: "Coding"
 };
 console.log(person);
 var fullName = person.firstName + " " + person.lastName;
 console.log('The person\'s name is:', fullName );
+var person = {
+  job: "Web Developer",
+  previousjob: "Student"
+}
 console.log('The person\'s current job is:', person.job);
-console.log('The person\'s previous job is:', person.previousJob);
+console.log('The person\'s previous job is:', person.previousjob);
 console.log(person);
