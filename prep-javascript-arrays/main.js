f1ded3c5780b357bbb8bbe4fb8d379200dceaf4f
Minji Cho <mturtle051@gmail.com>
2022-01-18 16:51:33 -0800

    completed prep-javascript-arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,10 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+var firtElement = [languages[0]];
+console.log('the first element of the array is:', firtElement);
+var thridElement = [languages[2]];
+console.log('the 3rd element of the array is:', thridElement);
+var length = languages.length;
+console.log('The length of the array is:',length);
+var lastElement = languages[length-1];
+console.log('The last element of the array is:',lastElement);
