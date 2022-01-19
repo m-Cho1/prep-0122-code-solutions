0ac5a50e945a6d09f0c029f9dcddd129e3729ed1
Minji Cho <mturtle051@gmail.com>
2022-01-18 16:27:05 -0800

    Edited complteted prep-javascript-object.

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -6,10 +6,8 @@ var person = {
 console.log(person);
 var fullName = person.firstName + " " + person.lastName;
 console.log('The person\'s name is:', fullName );
-var person = {
-  job: "Web Developer",
-  previousjob: "Student"
-}
+person.job = 'Web Developer';
+person.previousjob = 'Student';
 console.log('The person\'s current job is:', person.job);
 console.log('The person\'s previous job is:', person.previousjob);
 console.log(person);
