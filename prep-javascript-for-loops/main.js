49b241b5bb14e848911614e37ddfdd0fc40ac5c1
Minji Cho <mturtle051@gmail.com>
2022-01-20 17:37:54 -0800

    Completed prep-javascript-for-loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (let i = 0; i <10; i++) {
+  console.log(i);
+}
+for (let i=0; i<19; i+=2) {
+  console.log(i);
+}
+
+for (let i=100; i>=0; i--) {
+  console.log("time till explosion", i);
+
+}
